3c4fdc528d6e56a9b205a191a7c69dc6ed54c9dd
yuiii <yuiii@C010422915.zipangu.cac.co.jp>
2026-05-29 14:29:29 +0900

    Practice0402まで 修正

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q01/controller/Practice0401Controller.java
@@ -1,5 +1,36 @@
 package jp.co.sss.practice.p04.q01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p04.q01.form.BmiForm;
+
+@Controller
 public class Practice0401Controller {
+
+	//入力画面を表示するだけ
+	@RequestMapping(path = "/bmi/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice04/01/bmi_input";
+	}
+
+	//画面から送られてきた身長と体重の値を
+	//BmiFormというフォームクラスで受け取る
+	@RequestMapping(path = "/bmi/result", method = RequestMethod.POST)
+	public String result(BmiForm bmiForm, Model model) {
+
+		//受け取ったデータをもとにBMIを計算
+		Double weight = bmiForm.getWeight();
+		Double height = bmiForm.getHeight() / 100;
+		bmiForm.setBmi(weight / ((height) * (height)));
+
+		//BmiFormオブジェクトをModelに登録することで、画面側にデータを渡す
+		model.addAttribute("bmiForm", bmiForm);
+
+		//結果表示画面へ遷移
+		return "practice04/01/bmi_result";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q02/controller/Practice0402Controller.java
@@ -1,5 +1,53 @@
 package jp.co.sss.practice.p04.q02.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
 public class Practice0402Controller {
 
+	@RequestMapping(path = "/numguess/start", method = RequestMethod.GET)
+	public String start(HttpSession session) {
+
+		Integer hitNumber = (int) (Math.floor(Math.random() * 9) + 1);
+		session.setAttribute("answer", hitNumber);
+		System.out.println("当たりの数値: " + hitNumber);
+
+		return "practice04/02/numguess_start";
+
+	}
+
+	@RequestMapping(path = "/numguess/input", method = RequestMethod.GET)
+	public String input() {
+		return "practice04/02/numguess_input";
+	}
+
+	@RequestMapping(path = "/numguess/judge", method = RequestMethod.GET)
+	public String judge(HttpSession session, Integer inputNum) {
+		Integer answer = (Integer) session.getAttribute("answer");
+
+		if (answer.equals(inputNum)) {
+			return "redirect:/numguess/hit";
+		} else {
+			return "practice04/02/numguess_judge";
+		}
+
+	}
+
+	@RequestMapping(path = "/numguess/hit", method = RequestMethod.GET)
+	public String hit(Model model, HttpSession session) {
+
+		Integer answer = (Integer) session.getAttribute("answer");
+		String message = "当たりです！ 正解は" + answer + "でした。";
+
+		model.addAttribute("message", message);
+		session.removeAttribute("answer");
+
+		return "practice04/02/numguess_end";
+
+	}
 }
